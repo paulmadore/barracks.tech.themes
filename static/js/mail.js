f882878911aa87754b1108204d0d02cc15b5e54c
moonpunter@gmail.com <henrypaulmadore@gmail.com>
2015-12-29 15:23:42 -0600

    version 0.85

---
+++ static/js/mail.js
@@ -0,0 +1,10 @@
+function mail(url,windowName) {
+       newwindow=window.open(url,windowName,'height=250,width=500');
+       if (window.focus) {newwindow.focus()}
+       return false;
+     }
+function abt(url,windowName) {
+       newwindow=window.open(url,windowName,'height=350,width=650');
+       if (window.focus) {newwindow.focus()}
+       return false;
+     }     
